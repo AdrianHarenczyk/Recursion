d18071fd213b634e342b0ecf34546ee260faa8e2
Adrian Hareńczyk <adrian.harenczyk@gmail.com>
2018-05-22 14:30:29 +0200

    Fifth exercise done. SumDigits

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -14,7 +14,7 @@ public class Main {
         if (n == 0 || n == 1) {
             return 1;
         } else {
-            return n * factorial(n-1);
+            return n * factorial(n - 1);
         }
     }
 
@@ -26,23 +26,33 @@ public class Main {
         else if (n == 1 || n == 2)
             return 1;
         else
-            return fibonacci(n-1) + fibonacci(n-2);
+            return fibonacci(n - 1) + fibonacci(n - 2);
     }
+
     private int bunnyEars2(int bunnies) {
-        if (bunnies <=0)
+        if (bunnies <= 0)
             return 0;
         if (bunnies == 1)
             return 2;
         if (bunnies % 2 == 0)
-            return bunnyEars2(bunnies-1) + 3;
+            return bunnyEars2(bunnies - 1) + 3;
         else
-            return bunnyEars2(bunnies-1) + 2;
+            return bunnyEars2(bunnies - 1) + 2;
     }
+
     private int triangle(int rows) {
         if (rows <= 0)
             return 0;
         else
-            return triangle(rows-1) + rows;
+            return triangle(rows - 1) + rows;
+    }
+
+    private int sumDigits(int n) {
+        if (n / 10 == 0) {
+            return n;
+        } else {
+            return (n % 10) + sumDigits(n / 10);
+        }
     }
 
 }
