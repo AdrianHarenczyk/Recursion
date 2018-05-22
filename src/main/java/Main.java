4d45d78c291ad5fa051ed27052f28d753db9e21f
Adrian Hareńczyk <adrian.harenczyk@gmail.com>
2018-05-22 12:11:59 +0200

    Second exercise done. Fibonacci.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,7 @@ public class Main {
     public static void main(String[] args) {
         Main application = new Main();
         System.out.println(application.factorial(5));
+        System.out.println(application.fibonacci(8));
     }
 
 
@@ -11,10 +12,20 @@ public class Main {
             return -1;
         if (n == 0 || n == 1) {
             return 1;
-        } else if (n == 2) {
-            return 2;
         } else {
             return n * factorial(n-1);
         }
     }
+
+    private int fibonacci(int n) {
+        if (n < 0)
+            return -1;
+        else if (n == 0)
+            return 0;
+        else if (n == 1 || n == 2)
+            return 1;
+        else
+            return fibonacci(n-1) + fibonacci(n-2);
+    }
+
 }
