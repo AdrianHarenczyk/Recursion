7ba1dee466891d0ee197280f9095f6665cbdc8ce
Adrian Hareńczyk <adrian.harenczyk@gmail.com>
2018-05-23 08:48:59 +0200

    Exercise bunnyEars done still not done count7.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -64,4 +64,14 @@ public class Main {
             return 0;
     }
 
+    public int bunnyEars(int bunnies) {
+        if (bunnies <= 0)
+            return 0;
+        if (bunnies == 1)
+            return 2;
+        else
+            return bunnyEars2(bunnies - 1) + 2;
+    }
+
+
 }
