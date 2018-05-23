c5a1be79a98f3471d4e9d40c6316ee98b97f6e7d
Adrian Hareńczyk <adrian.harenczyk@gmail.com>
2018-05-23 11:10:25 +0200

    Done change XY.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -102,13 +102,22 @@ public class Main {
         return countX(str.substring(0, str.length() - 1));
     }
 
-    public int countHi(String str) {
+    private int countHi(String str) {
         if (str == null || str.length() < 2)
             return 0;
         if (str.substring(str.length() - 2, str.length()).equals("hi")) {
             return 1 + countHi(str.substring(0, str.length() - 2));
         }
         return countHi(str.substring(0, str.length() - 1));
+    }
+    private String changeXY(String str) {
+        if (str == null || str.equals("")) {
+            return "";
+        }
+        if (!str.contains("x")) {
+            return str;
+        }
+        return changeXY(str.replaceFirst("x","y"));
     }
 
 }
