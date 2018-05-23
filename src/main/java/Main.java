de50bd3e01d0a981eb36ac5368ef39b206e5fe8d
Adrian Hareńczyk <adrian.harenczyk@gmail.com>
2018-05-23 10:08:57 +0200

    Trying to do countHi

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -99,5 +99,14 @@ public class Main {
         }
         return countX(str.substring(0, str.length() - 1));
     }
+    private int countHi(String str) {
+        if (str.length() == 1) {
+            return 0;
+        }
+        if (str.charAt(str.length()-1)=='i' && str.charAt(str.length()-2) == 'h') {
+            return 1+countHi(str.substring(0,str.length()-2));
+        }
+        return countHi(str.substring(0,str.length()-2));
+    }
 
 }
